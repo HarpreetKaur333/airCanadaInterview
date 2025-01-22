f332dc22a61f8b75bd0bc304c17919893ba889f2
HarpreetKaur <dhillonharpreet333@gmail.com>
2025-01-21 22:53:11 -0500

    #fixed

--- src/app/impure-pipe.pipe.ts
+++ src/app/impure-pipe.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'impurePipe',
+  name: 'filter',
   standalone: true,
   pure: false // This makes the pipe impure
 })
--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { TruncatePipePipe } from '../truncate-pipe.pipe';
 import { ImpurePipePipe } from '../impure-pipe.pipe';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports:  [TruncatePipePipe,ImpurePipePipe],
+  imports:  [TruncatePipePipe, FormsModule, CommonModule, ImpurePipePipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
@@ -14,6 +16,7 @@ export class PipeComponent {
     return value.length > limit ? value.substring(0, limit) + '...' : value;
   }
   longText: string = 'This is a very long text that should be truncated';
+
   searchText: string = '';
   items = [
     { name: 'Apple' },
